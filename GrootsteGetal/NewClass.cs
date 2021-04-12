97d67224cb75fe66f9f0505f33aeddded598a37b
Jesse <jesvis@bitconsult.be>
2021-04-12 15:51:38 +0200

    Add project files.

---
+++ GrootsteGetal/NewClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrootsteGetal
+{
+    public class NewClass
+    {
+        public string kleur { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
---
+++ GrootsteGetal/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GrootsteGetal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime t = DateTime.Now;
+            Console.WriteLine(t);
+        }
+    }
+}
